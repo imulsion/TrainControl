7a7c0d9b49eee1e2ac7087909f24fe0db928134c
imulsion <sk6g16@soton.ac.uk>
2019-04-15 18:58:35 +0100

    Fixed issue with makefile after renaming /build to /bin

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
 cd src
 x86_64-w64-mingw32-g++ implementation.cpp main.cpp -o main.exe -std=c++11 -I "../include" -L "../libs" -lftd2xx -lftd2xx64
-move main.exe ../build
+move main.exe ../bin/main.exe
 cd ..
