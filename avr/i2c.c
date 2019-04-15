f132a29cda1bec24a6b726f0fdc75fe59aa9edf9
imulsion <sk6g16@soton.ac.uk>
2019-04-15 18:54:07 +0100

    Further development of AVR code in preparation for section management integration

--- avr/i2c.c
+++ avr/i2c.c
@@ -1,18 +1,40 @@
+ 
+/*****************************************************************************************
+ * 				          MACROS                                         *
+ *****************************************************************************************/
 #include <avr/interrupt.h> //includes avr/io.h
 #include "i2c.h"
 
 
+/*****************************************************************************************
+ * 				        DEFINITIONS                                      *
+ *****************************************************************************************/
+//globals for accessing SectionT structs in functions other than main
+volatile SectionT* sectionOnePtr;
+volatile SectionT* sectionTwoPtr;
+
+volatile uint8_t globalData;                               //used for receiving I2C data
+volatile uint8_t dataReady;                                //flag to indicate data received
+
+volatile I2CReadStateT readState = I2C_IDLE;
+
+
+/*****************************************************************************************
+ * 	               	        INTERRUPT SERVICE ROUTINES                               *
+ *****************************************************************************************/
+
+//NOTE: Interrupts do not have to be manually disabled and enabled during ISRs. The hardware automatically disables and enables interrupts
+//at the start and end of all ISRs.
+
 /*
- * Function: ISR(I2C_vect)
- *
- * I2C interrupt routine. Function called whenever the TWINT flag in I2C_CONTROL_REGISTER is set (to 0) by hardware. This occurs when an I2C event occurs such as a START or STOP condition. The TWINT flag must then be cleared (set to 1) by software to allow the hardware to continue receiving events
+ * I2C interrupt routine. Function called whenever the TWINT flag in I2C_CONTROL_REGISTER is set (to 0) by hardware. This occurs when an I2C 
+ * event occurs such as a START or STOP condition. The TWINT flag must then be cleared (set to 1) by software to allow the hardware to 
+ * continue receiving events
  *
  * Parameter[in] I2C_vect 		Vector specifying the interrupt being serviced.
  */
 ISR(I2C_vect) 
 {
-	cli();//disable interrupts while processing current interrupt
-
 	uint8_t status = I2C_STATUS_REGISTER & I2C_PRESCALER_MASK;   //extract the event type from the I2C status register
 	switch(status)
 	{
@@ -21,9 +43,54 @@ ISR(I2C_vect)
 			break;
 
 		//AVR has been addressed with SLA+W, hardware will wait for next event, no software action required
-		case I2C_ADDRESSED:
+		case I2C_ADDRESSED_W:
 			break;
 
+		//AVR has been addressed with SLA+R, PC is requesting a status report
+		case I2C_ADDRESSED_R:
+			readState = I2C_SECTION_ONE_OCCUPANCY;
+			I2C_DATA_REGISTER = sectionOnePtr->trainInSection;
+			break;
+
+		//Last byte sent received, ACK returned. Next byte should be sent
+		case I2C_READING_ACK:
+			//decide which byte to send next
+			switch(readState)
+			{
+				case I2C_SECTION_ONE_OCCUPANCY:
+					readState = I2C_SECTION_ONE_DCYCLE;
+					I2C_DATA_REGISTER = sectionOnePtr->dutyCycle;
+					break;
+				case I2C_SECTION_ONE_DCYCLE:
+					readState = I2C_SECTION_TWO_OCCUPANCY;
+					I2C_DATA_REGISTER = sectionTwoPtr->trainInSection;
+					break;
+				case I2C_SECTION_TWO_OCCUPANCY:
+					readState = I2C_SECTION_TWO_DCYCLE;
+					I2C_DATA_REGISTER = sectionTwoPtr->dutyCycle;
+					break;
+				//if this case is executed, the master is asking for too many bytes of data. Generate a NACK to refuse to send more
+				//NOTE: Master should have sent a NACK upon receipt of the previous byte, not an ACK. This is why this 
+				//state is an error state.
+				case I2C_SECTION_TWO_DCYCLE:
+					readState = I2C_IDLE;
+					I2C_CONTROL_REGISTER |= I2C_TWSTO_MASK;
+					break;
+				//only untested case is I2C_IDLE, it should be impossible to get here. Send NACK if this somehow executes
+				default:
+					readState = I2C_IDLE;
+					I2C_CONTROL_REGISTER |= I2C_TWSTO_MASK;
+					break;
+			}
+
+		//Last byte sent has been received, NACK returned. Transmission will terminate.
+		case I2C_READING_NACK:
+			//transmission will terminate, regardless of whether all the required bytes have been requested by master. 
+			//NOTE: The slave doesn't care if the master doesn't get a full status report. If this is not handled at the master side
+			// it may be possible for the master to receive corrupted or incomplete status data.
+			
+			readState = I2C_IDLE; //reset state machine
+			break;
 
 		//AVR is being written to over I2C, read the data
 		case I2C_WRITING:
@@ -31,10 +98,6 @@ ISR(I2C_vect)
 			dataReady = DATA_WAITING;
 			break;
 
-		//computer has received data in I2C_DATA_REGISTER and sent ACK, no action needed
-		case I2C_READING:
-			break;
-			
                 //unexpected status code
 		default: 
 			PORTB = status;//write error code to PORTB
@@ -42,13 +105,41 @@ ISR(I2C_vect)
 	}
 
 	I2C_CONTROL_REGISTER |= I2C_INTERRUPT_CLEAR_MASK;//clear TWINT bit so hardware can continue receiving events
-
-	sei();//re-enable global interrupts
 }
 
+/*
+ * External interrupt routine from the INT0 pin. Function called whenever there is a logical change on the INT0 pin, 
+ * indicating a train has left or entered the section.
+ *
+ * Parameter[in] INT0_vect              Vector specifying the interrupt being serviced
+ */ 
+ISR(INT0_vect) 
+{
+	sectionOnePtr -> trainInSection = SECTION_OCCUPANCY_MASK & ~(sectionOnePtr -> trainInSection); //toggle occupancy status of the section
+}
 
+/*
+ * External interrupt routine from the INT1 pin. Function called whenever there is a logical change on the INT1 pin, 
+ * indicating a train has left or entered the section.
+ *
+ * Parameter[in] INT1_vect              Vector specifying the interrupt being serviced
+ */ 
+ISR(INT1_vect)
+{
+	sectionTwoPtr -> trainInSection = SECTION_OCCUPANCY_MASK & ~(sectionTwoPtr -> trainInSection); //toggle occupancy status of the section
+}
+
+/*****************************************************************************************
+ * 				      MAIN FUNCTION                                      *
+ *****************************************************************************************/
 int main (void)
 {
+	SectionT sectionOne;
+	SectionT sectionTwo;
+	sectionOnePtr = &sectionOne;
+	sectionTwoPtr = &sectionTwo;
+	sectionOne.dutyCycle = PWM_INITIAL_DUTY_CYCLE;
+	sectionOne.dutyCycle = PWM_INITIAL_DUTY_CYCLE;
 	DeviceInit();//initialise internal registers and set up device	
 	
        	sei();//enable global interrupts 
@@ -59,15 +150,18 @@ int main (void)
 		//if data has been received
 		if(DATA_WAITING == dataReady)
 		{
+			cli(); //disable interrupts while dealing with interrupt modifiable data
 			PWM_COUNTER_REGISTER = globalData;//change PWM duty cycle
-			cli();
 			dataReady = NOT_DATA_WAITING;
 			sei();
 		}
 	}
-	return 0;
+	return 0; //main
 }
 
+/*****************************************************************************************
+ *          		          FUNCTION DEFINITIONS                                   *
+ *****************************************************************************************/
 void DeviceInit(void)
 {
 	//global variables
@@ -77,14 +171,36 @@ void DeviceInit(void)
 	//port registers and initial value setup 
 	PORTB_DATA_DIRECTION_REGISTER |= PORTB_DATA_DIRECTION_VALUE;
 	PORTB &= REGISTER_CLEAR_MASK;
+	PORTD_DATA_DIRECTION_REGISTER |= PORTD_DATA_DIRECTION_VALUE;
+	PORTD &= REGISTER_CLEAR_MASK;
+
+	//PWM initialisation
+	PWM_CONTROL_REGISTER_TWO = PWM_CONTROL_CONFIG; 
+	PWM_COUNTER_REGISTER = PWM_INITIAL_DUTY_CYCLE;
+	
 	
 	//I2C initialisation
 	I2C_BITRATE_REGISTER &= REGISTER_CLEAR_MASK; //ensure bit rate register is cleared
 	I2C_ADDRESS_REGISTER = I2C_ADDR;//assign I2C address to I2C address register
 	I2C_CONTROL_REGISTER = I2C_CONTROL_CONFIG; //set i2c configuration register
 	
-	//PWM initialisation
-	PWM_CONTROL_REGISTER_TWO = PWM_CONTROL_CONFIG; 
-	PWM_COUNTER_REGISTER = PWM_INITIAL_DUTY_CYCLE;
-	
+	uint8_t pinStatus = PIND; //get section occupation status
+
+	//set SectionT status based on section occupation
+	if(INT0_SECTION_SENSE_MASK == (INT0_SECTION_SENSE_MASK & pinStatus))
+	{
+                sectionOnePtr->trainInSection = SECTION_OCCUPIED;
+	}
+	else
+	{
+		sectionOnePtr->trainInSection = SECTION_FREE;
+	}
+	if(INT1_SECTION_SENSE_MASK == (INT1_SECTION_SENSE_MASK & pinStatus))
+	{
+                sectionTwoPtr->trainInSection = SECTION_OCCUPIED;
+	}
+	else
+	{
+		sectionTwoPtr->trainInSection = SECTION_FREE;
+	}
 }
--- avr/i2c.h
+++ avr/i2c.h
@@ -1,3 +1,10 @@
+#ifndef I2C_H //header guard
+#define I2C_H
+
+/*****************************************************************************************
+ * 				          MACROS                                         *
+ *****************************************************************************************/
+
 //compile with -D SLAVE_ONE if compiling for first slave, SLAVE_TWO if compiling for second
 #ifdef SLAVE_ONE
 	#define I2C_ADDR 0x02U;//i2c slave address
@@ -17,6 +24,10 @@
 //Port constant definitions
 #define PORTB_DATA_DIRECTION_REGISTER    DDRB
 #define PORTB_DATA_DIRECTION_VALUE       0xFFU             //PORTB pins data direction (all outputs)       
+#define PORTD_DATA_DIRECTION_REGISTER    DDRD
+#define PORTD_DATA_DIRECTION_VALUE       0xF3U             //PORTD data direction (all outputs except PD2 and PD3
+#define INT0_SECTION_SENSE_MASK          0x04U             //mask to detect a train in the section connected to INT0
+#define INT1_SECTION_SENSE_MASK          0x08U             //mask to detect a train in the section connected to INT1
 
 
 //I2C constant definitions
@@ -26,30 +37,74 @@
 #define I2C_CONTROL_REGISTER             TWCR              //Alias for I2C control register
 #define I2C_ADDRESS_REGISTER             TWAR              //Alias for I2C address register
 #define I2C_BITRATE_REGISTER             TWBR              //Alias for I2C bit rate register
+#define I2C_TWSTO_MASK                   0x10U             //Mask for setting TWSTO in TWCR in case of an error while sending data to the master
 #define I2C_CONTROL_CONFIG		 0xC5U             //I2C control register configuration bits
 #define I2C_INTERRUPT_CLEAR_MASK	 0x80U             //used for clearing I2C interrupt flag 
 #define I2C_PRESCALER_MASK		 0xF8U             //mask for TWSR to remove prescaler bits
-#define I2C_ADDRESSED			 0x60U             //status code for receiving own SLA+W
+#define I2C_ADDRESSED_W			 0x60U             //status code for receiving own SLA+W and ACK returned
 #define I2C_WRITING			 0x80U             //status code for receipt of data and ACK returned
-#define I2C_READING			 0xA8U             //status code for receipt of ACK after a write to the PC
+#define I2C_ADDRESSED_R			 0xA8U             //status code for receipt of SLA+R and ACK returned
+#define I2C_READING_ACK                  0xB8U             //status code for successful transmission from TWDR, ACK returned from computer
+#define I2C_READING_NACK                 0xC0U             //status code for successful transmission from TWDR, NACK returned, communication will be terminated by master
 #define I2C_STOP_OR_REPEATED_START	 0xA0U             //status code for receipt of I2C repeated start or stop condition
 
 //PWM constant definitions
 #define PWM_CONTROL_REGISTER_TWO         TCCR2             //Alias for 2nd PWM channel control register
 #define PWM_PRESCALER                    1U                //Value for the PWM prescaler. Equation for PWM frequency is F = F_CPU/(510*PWM_PRESCALER)
 #define PWM_CONTROL_CONFIG               0x61U             //PWM control register configuration bits 
-#define PWM_COUNTER_REGISTER             OCR2             //Internal counter register used by the PWM hardware. The value written to this register determines the duty cycle of the PWM waveform
+#define PWM_COUNTER_REGISTER             OCR2              //Internal counter register used by the PWM hardware. The value written to this register determines the duty cycle of the PWM waveform
 #define PWM_INITIAL_DUTY_CYCLE           128U              //Initial duty cycle value (50%)
 
-volatile uint8_t globalData;                               //used for receiving I2C data
-volatile uint8_t dataReady;                                //flag to indicate data received
+//External interrupt definitions
+#define MCU_CONTROL_REGISTER             MCUCR             //AVR main control register
+#define INTERRUPT_CONTROL_REGISTER       GICR              //AVR interrupt control register
+#define MCU_CONFIG                       0x05U             //generate external interrupts on any logical change on both INT pins
+#define INTERRUPT_CONTROL_CONFIG         0xC0U             //enable external interrupts on both pins
 
+//train occupancy definitions and masks
+#define SECTION_OCCUPIED                 0x01U             //indicates an occupied section
+#define SECTION_FREE                     0x00U             //indicates an unoccupied section
+#define SECTION_OCCUPANCY_MASK           0x01U             //mask for toggling between SECTION_OCCUPIED and SECTION_FREE
+
+
+
+/*****************************************************************************************
+ * 				          TYPES                                          *
+ *****************************************************************************************/
+
+//struct for storing data about a section owned by the controller
+typedef struct
+{
+	uint8_t trainInSection;
+	uint8_t dutyCycle;
+}SectionT; 
+
+//enum describing a simple state machine for the correct ordering of transmission of status bytes to the computer
+typedef enum
+{
+	I2C_IDLE,
+	I2C_SECTION_ONE_OCCUPANCY,
+	I2C_SECTION_ONE_DCYCLE,
+	I2C_SECTION_TWO_OCCUPANCY,
+	I2C_SECTION_TWO_DCYCLE,
+}I2CReadStateT;
+
+
+/*****************************************************************************************
+ * 				       DEFINITIONS                                       *
+ *****************************************************************************************/
+
+ 
+/*****************************************************************************************
+ * 			           FUNCTION PROTOTYPES                                   *
+ *****************************************************************************************/
 /*
- * Function: I2CInit
- *
  * I2C initialisation function. Sets up registers for I2C configuration with appropriate values. 
  *
- * No parameter and no return value
+ * No parameters or return value
  *
  */
 void DeviceInit(void);
+
+
+#endif //I2C_H
